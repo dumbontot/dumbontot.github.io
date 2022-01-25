c7e7f51f0aa5a8b6291789b561a957a0f77b9017
shak <68632534+dumbontot@users.noreply.github.com>
2022-01-25 18:36:23 +0800

    Create feedback.js

---
+++ feedback.js
@@ -0,0 +1,18 @@
+    <script type="text/javascript">
+      $(document).ready(function(){
+         $('#comment').click(function(){
+          var input= $('#input').val();
+          $('.box').append(input +'<br>');
+          $('#input').val('');
+          $('.box-container').slideDown();
+         });
+          
+         $('#cancel').click(function(){
+          $('#input').val('');
+         });
+       
+         $('#delete').click(function(){
+          $('.box').text('');
+          $('.box-container').slideUp();
+         });
+      });
